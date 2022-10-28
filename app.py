d7745561f9b2ba7ea330adfcecd97785613776bd
Trevor Royer <troyer@redhat.com>
2022-10-28 10:31:54 -0500

    Add app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+"""Main entrypoint for container startup."""
+from hello_world.main import app
+
+import uvicorn
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8000,
+    )
